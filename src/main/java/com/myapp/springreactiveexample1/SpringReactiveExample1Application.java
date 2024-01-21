22743a82ed2b242c147c0913defc2d95bd9e21af
Nuwan <nuwan199044@gmail.com>
2024-01-21 14:56:15 +0530

    Initial project

---
+++ src/main/java/com/myapp/springreactiveexample1/SpringReactiveExample1Application.java
@@ -0,0 +1,13 @@
+package com.myapp.springreactiveexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveExample1Application.class, args);
+	}
+
+}
